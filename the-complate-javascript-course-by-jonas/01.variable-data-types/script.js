1df9d080c685b4dad1c2894420fbcdd765c0756e
Foysal Hossain <fimran2011@gmail.com>
2019-09-12 08:47:47 +0600

    variable js

---
+++ the-complate-javascript-course-by-jonas/01.variable-data-types/script.js
@@ -0,0 +1,20 @@
+
+// PREMITIVE DATA TYPES: Number, String, Boolean, Undefined, Null
+
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith'; // string
+var age = 28; // number
+
+var fullAge = true; // Boolean
+console.log(fullAge);
+
+var job; // Undefined
+console.log(job);
+
+job = 'teacher';
+console.log(job);
+
+isMerried = null;
+console.log(isMerried); // Null
