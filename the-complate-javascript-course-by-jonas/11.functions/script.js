b4cae04f26c108823d609cf21547aa9867a0d3fd
Foysal Hossain <fimran2011@gmail.com>
2019-09-14 10:07:23 +0600

    14-09-2019

---
+++ the-complate-javascript-course-by-jonas/11.functions/script.js
@@ -0,0 +1,28 @@
+/************************************************
+* Functions
+*/
+
+function calculateAge(birthYear) {
+    return 2018 - birthYear;
+}
+
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1948);
+var ageJane = calculateAge(1969);
+console.log(ageJohn, ageMike, ageJane);
+
+
+function yearsUntilRetirement(year, firstName) {
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+
+    if(retirement > 0) {
+        console.log(firstName + ' retires in ' + retirement + ' years.'); 
+    } else {
+        console.log(firstName + ' is already reatired.');
+    }
+}
+
+yearsUntilRetirement(1990, 'John');
+yearsUntilRetirement(1948, 'Mike');
+yearsUntilRetirement(1969, 'Jane');
