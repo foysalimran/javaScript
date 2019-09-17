88422a48854f9b03ade7393f7f43366d83d96fdf
Foysal Hossain <fimran2011@gmail.com>
2019-09-17 10:17:24 +0600

    16-09-2019

---
+++ the-complate-javascript-course-by-jonas/18.loops-and-itearation/script.js
@@ -0,0 +1,16 @@
+/*****************************************
+ * Loops and iteration
+ */
+
+ // For loop
+
+ for (var i = 0; i < 10; i++) {
+     console.log(i);
+ }
+ 
+ var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+
+ for (var i = 0; i < john.length; i++) {
+     console.log(john[i]);
+ }
+ 
