583ec5a72ef531b08d6de02e0ad2a99264c035c9
Foysal Hossain <fimran2011@gmail.com>
2019-09-17 10:03:03 +0600

    17-10-2019

---
+++ the-complate-javascript-course-by-jonas/17.coading-challange/script.js
@@ -0,0 +1,35 @@
+/***************************************
+ * Coding challenge
+ */
+
+
+var john = {
+    fullName: 'John Smith',
+    mass : 92,
+    height: 1.95,
+    calcBMI: function() {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+}
+
+var mark = {
+    fullName : 'Mark Miller',
+    mass : 178,
+    height: 1.69,
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+}
+
+john.calcBMI();
+mark.calcBMI();
+
+if (john.calcBMI() > mark.calcBMI()) {
+    console.log(john.fullName + ' has a higher BMI of ' + john.bmi);
+} else if (mark.bmi > john.bmi) {
+    console.log(mark.fullName + ' has a higher BMI of ' + mark.bmi);
+} else {
+    console.log('They have the same BMI');
+}
