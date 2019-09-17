5c8d032562343c956559b32b5dd6cb81871368d2
Foysal Hossain <fimran2011@gmail.com>
2019-09-17 10:27:25 +0600

    16-10-19

--- the-complate-javascript-course-by-jonas/18.loops-and-itearation/script.js
+++ the-complate-javascript-course-by-jonas/18.loops-and-itearation/script.js
@@ -3,7 +3,7 @@
  */
 
  // For loop
-
+/*
  for (var i = 0; i < 10; i++) {
      console.log(i);
  }
@@ -14,3 +14,31 @@
      console.log(john[i]);
  }
  
+
+ // While Loop
+ var i = 0;
+ while (i < john.length) {
+    console.log(john[i]);
+    i++;
+ }
+*/
+ // continue and break statements
+
+ var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+
+ for (var i = 0; i < john.length; i++) {
+     if (typeof john[i] !== 'string') continue;
+     console.log(john[i]);
+ }
+
+
+ for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') break;
+    console.log(john[i]);
+}
+
+// Looping backward
+for (var i = john.length - 1; i >= 0; i--) {
+    console.log(john[i]);
+    
+}
