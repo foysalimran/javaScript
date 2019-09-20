7dbcd3810fc1b775abacbc2a367ceb136fed51a3
Foysal Hossain <fimran2011@gmail.com>
2019-09-20 14:35:34 +0600

    20-09-2019

---
+++ basic-js/script.js
@@ -0,0 +1,30 @@
+/**************************************
+ * Basic javascript
+ */
+
+ /********************************************
+  * 01. Start coding
+  */
+
+console.log('Hello world!!!');
+
+/********************************************
+* 02. Variables and data types
+*/
+
+var firstName = 'John';
+console.log(firstName);
+
+lastName = 'Smith'; // String: Sequence of characters, used for text
+var age = 28;  // Number: Floating point numbers, for decimals and integers
+
+var fullAge = true; // Boolean: Logical data type that can only be true or false
+console.log(fullAge);
+
+var job; // Undefined: Data type of a variable that does not have a value yet
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+// Null: Also mean 'non-existent'
