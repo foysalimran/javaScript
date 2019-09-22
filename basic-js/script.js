4801306c659d2ed49bd080537dadd9db05e33779
Foysal Hossain <fimran2011@gmail.com>
2019-09-22 09:20:05 +0600

    Update script.js
    22-09-2019

--- basic-js/script.js
+++ basic-js/script.js
@@ -28,3 +28,87 @@ job = 'Teacher';
 console.log(job);
 
 // Null: Also mean 'non-existent'
+
+/********************************************
+* 03. Basic operators
+*/
+var now, yearJohn, yearMark;
+now = 2020;
+ageJohn = 28;
+ageMark = 33;
+
+// Math operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+// Logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
+
+
+// Operator precedence
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+// Multiple operators
+var isFullAge = now - yearJohn >= fullAge; // true
+console.log(isFullAge);
+
+// Grouping
+var ageJohn = now -yearJohn;
+var ageMark = 35;
+var avarage = (ageJohn + ageMark) / 2;
+console.log(avarage);
+
+// Multiple assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
+console.log(x, y);
+
+// More operators
+
+x *= 2;
+console.log(x);
+x += 10;
+console.log(x);
+
+x++;
+console.log(x);
+
+x--;
+console.log(x);
+
+/****************************
+* Coding challenge 1
+*/
+var massMark = 78; // kg
+var heightMark = 1.69; // meters
+
+var massJohn = 92;
+var heightJohn = 1.95;
+
+var BMIMark = massMark / (heightMark * heightMark);
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+
+var markHeigherBMI = BMIMark > BMIJohn;
+console.log('Is Mark\'s BMI higher than john\'s? ' + markHeigherBMI);
+
+// If else statement
+
