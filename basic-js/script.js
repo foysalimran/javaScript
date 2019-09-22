87233f1a715a8d9f0634e1829150cb8af00a95ec
Foysal Hossain <fimran2011@gmail.com>
2019-09-22 10:12:13 +0600

    22-09-2019

--- basic-js/script.js
+++ basic-js/script.js
@@ -112,3 +112,51 @@ console.log('Is Mark\'s BMI higher than john\'s? ' + markHeigherBMI);
 
 // If else statement
 
+var firstName = 'John';
+var civilStatus = 'single';
+
+if (civilStatus === 'married') {
+  console.log(firstName + ' is married!');
+} else {
+  console.log(firstName + ' will hopefully marry soon :)');
+}
+
+var isMarried = true;
+if (isMarried) {
+  console.log(firstName + ' is married!');
+} else {
+  console.log(firstName + ' will hopefully marry soon :)');
+}
+
+var massMark = 78; // kg
+var heightMark = 1.69; // meters
+
+var massJohn = 92;
+var heightJohn = 1.95;
+
+var BMIMark = massMark / (heightMark * heightMark);
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+
+if (BMIMark > BMIJohn) {
+  console.log('Mark\'s BMI is higher than John\'s BMI');
+} else {
+  console.log('John\'s BMI is higher than Mark\'s BMI');
+}
+
+/********************************************
+ * Boolean Logic
+ */
+
+ var firstName = 'John';
+ var age = 20;
+
+ if (age < 16) {
+   console.log(firstName + ' is a boy.');
+ } else if (age >= 13 && age < 20 ) {
+  console.log(firstName + ' is a teenager.');
+ } else if (age >= 20 && age < 30) {
+  console.log(firstName + ' is a young man.');
+  
+ } else {
+   console.log(firstName + ' is a man.');
+ }
