5eb84e6e400e555e9ddf6183f09913a7a5b7372e
Foysal Hossain <fimran2011@gmail.com>
2019-09-23 10:06:25 +0600

    23-09-2019

--- basic-js/script.js
+++ basic-js/script.js
@@ -156,7 +156,107 @@ if (BMIMark > BMIJohn) {
   console.log(firstName + ' is a teenager.');
  } else if (age >= 20 && age < 30) {
   console.log(firstName + ' is a young man.');
-  
  } else {
    console.log(firstName + ' is a man.');
  }
+
+/********************************************
+ * The Ternary Operator and Switch Statements
+ */
+
+
+ var firstName = 'John';
+ var age = 16;
+// Ternary operatior
+ age >= 18 ? console.log(firstName + ' drinks beer') : console.log(firstName + ' drinks juice');
+ 
+ var drink = age >= 18 ? console.log('beer') : 'juice';
+ console.log(drink);
+ 
+//  if (age => 18) {
+//    var drink = 'beer';
+//  } else {
+//    var drink = 'juice';
+//  }
+
+
+ // Switch Statement
+ var job = 'instractor';
+ switch (job) {
+   case 'teacher':
+    case 'instractor':
+    console.log(firstName + ' teaches kids how to code.');
+    break;
+  case 'driver':
+    console.log(firstName + ' drives an uber in Lisbon');
+    break;
+  case 'designer':
+    console.log(firstName + ' designs website beautiful websites.');
+    break;
+  default:
+    console.log(firstName + ' does something else.');
+ }
+
+ switch (true) {
+   case age < 16:
+      console.log(firstName + ' is a boy.');
+    break;
+    case age >= 13 && age < 20:
+        console.log(firstName + ' is a teenager.');
+    break;
+    case age >= 20 && age < 30:
+        console.log(firstName + ' is a young man.');
+    break;
+    default:
+    console.log(firstName + ' is a man.');
+ }
+
+ /********************************************
+ * Truthy and Falsy values and equality operators
+ */
+
+ // falsy values: undefined, null, 0, '', NaN
+ // truthy values: NOT falsy values
+
+ var height;
+height = 23;
+ if (height || height === 0) {
+   console.log('Variable is defined');
+ } else {
+   console.log('Variable has not been defined.');
+ }
+
+
+ // Equality Operators
+ if (height === '23') {
+   console.log('The == operator does type coerction');
+ }
+
+
+ // Coding challenge
+
+ scoreJohn = (89 + 120 + 103) / 3;
+ scoreMike = (116 + 94 + 123) / 3;
+ scoreMary = (97 + 134 + 105) / 3;
+
+ console.log(scoreJohn, scoreMike, scoreMary);
+
+ if (scoreJohn > scoreMike && scoreJohn > scoreMary) {
+  console.log('John\'s team win with ' + scoreJohn + ' points.');
+} else if (scoreMike > scoreJohn && scoreJohn > scoreMary) {
+  console.log('Mike\'s team win with ' + scoreMike + ' points.');
+} else if (scoreMary > scoreJohn && scoreMary > scoreMike){
+  console.log('Mary\'s team win with ' + scoreMary + ' points.');
+} else {
+  console.log('There is a draw!');
+}
+
+ /*
+ if (scoreJohn > scoreMike) {
+  console.log('John\'s team win with ' + scoreJohn + ' points.');
+ } else if (scoreMike > scoreJohn) {
+  console.log('Mike\'s team win with ' + scoreMike + ' points.');
+ } else {
+   console.log('There is a draw!');
+ }
+*/
