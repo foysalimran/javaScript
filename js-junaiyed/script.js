712deb8a0bedab8934f35cdd00e8c8e3adf9b3ce
Foysal Hossain <fimran2011@gmail.com>
2019-10-08 10:38:07 +0600

    8-10-2019

---
+++ js-junaiyed/script.js
@@ -0,0 +1,80 @@
+console.log('Hello world from inside!');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log('Wrong!');
+
+// Variables
+
+var myName = 'Foysal Imran';
+console.log(myName);
+
+
+// Data type
+
+/**
+ * * There are 2 types of data types in javaScript.
+ * 1. primitive Data Types
+ *  - Number
+ *  - String
+ *  - Booliean
+ *  - Undefined
+ *  - Null
+ * 2. Non-primitive Data Types
+ *  - Array
+ *  - Object
+ *  - Function
+*/
+
+// Number primitive data type
+
+var aNumber = 10;
+var anotherNumber = 10.10;
+
+// String primitive data type
+
+var text = 'I want to say something';
+var text2 = "This exactly the same way, but use either one";
+var isString = '10'; // Is also a string not a number.
+
+
+// Booliean primitive data type
+
+isTrue = true;
+isFalse = false;
+
+
+// Undefined
+
+var something;
+console.log(something);
+
+// Null
+
+var some_thing = null;
+console.log(something);
+
+
+// CONCATENATION
+
+var text = 'hello' + ' there';
+console.log(text);
+
+
+var welcomeMsg = 'Hi! ' + text;
+console.log(welcomeMsg);
+
+console.log(typeof(welcomeMsg));
