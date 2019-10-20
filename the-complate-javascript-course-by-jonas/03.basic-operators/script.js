bc524e766e3334ac375a2288ce0527a3bbd4deda
Foysal Hossain <fimran2011@gmail.com>
2019-10-20 18:36:24 +0600

    19-10-2019

--- the-complate-javascript-course-by-jonas/03.basic-operators/script.js
+++ the-complate-javascript-course-by-jonas/03.basic-operators/script.js
@@ -1,15 +1,14 @@
-/*******************************
-* Basic Operators
-*/
-
+/**************************
+ * Basic Operators
+ */
 var now, yearJohn, yearMark;
-now = 2020;
+now = 2019;
 ageJohn = 28;
-ageMark = 33;
+ageMark = 23;
 
 // Math Operators
-yearJohn = now - 28;
-yearMark = now - 33;
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
 
 console.log(yearJohn);
 
@@ -17,17 +16,14 @@ console.log(now + 2);
 console.log(now * 2);
 console.log(now / 10);
 
+
 // Logical Operators
-var johnOlder = ageJohn < ageMark;
+var johnOlder = ageJohn > ageMark;
 console.log(johnOlder);
 
-// typeof operators
-console.log(typeof johnOlder);
+// Typeof operator
+console.log(typeof(johnOlder));
 console.log(typeof ageJohn);
-console.log(typeof 'Mark is older than John');
+console.log( typeof('Mark is older than John'));
 var x;
-console.log(typeof x);
-
-
-
-
+console.log(typeof(x));
