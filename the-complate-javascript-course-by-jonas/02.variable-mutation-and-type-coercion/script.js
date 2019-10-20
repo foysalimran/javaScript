1155e40e0061e456624d015e60ca846bfdc53694
Foysal Hossain <fimran2011@gmail.com>
2019-10-20 18:15:16 +0600

    20-10-2019

--- the-complate-javascript-course-by-jonas/02.variable-mutation-and-type-coercion/script.js
+++ the-complate-javascript-course-by-jonas/02.variable-mutation-and-type-coercion/script.js
@@ -6,19 +6,21 @@ var firstName = 'John';
 var age = 28;
 
 // Type coercion
+
 console.log(firstName + ' ' + age);
 
 var job, isMarried;
-job = 'Teacher';
+job = 'teacher';
 isMarried = false;
 
-console.log(firstName + ' is a ' + age + ' year old' + job + '. Is he married? ' + isMarried);
+console.log(firstName  + '  is 24 years old ' + job + '. Is he married? ' + isMarried);
 
 // Variable mutation
 age = 'twenty eight';
 job = 'driver';
-alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+
+alert(firstName  + '  is 24 years old ' + job + '. Is he married? ' + isMarried);
 
 var lastName = prompt('What is his last name?');
 console.log(firstName + ' ' + lastName);
-
