820b65f8fb6ff378ab9b2f595ff897b59a060e4a
Foysal Hossain <fimran2011@gmail.com>
2019-10-20 17:28:01 +0600

    20-10-2019

--- the-complate-javascript-course-by-jonas/01.variable-data-types/script.js
+++ the-complate-javascript-course-by-jonas/01.variable-data-types/script.js
@@ -1,24 +1,41 @@
+console.log('Hello World!!!');
 
-/***********************************
-* Variables and data types
-*/
+/************************
+ * Variables and Data Types
+ * 
+ */
 
-// PREMITIVE DATA TYPES: Number, String, Boolean, Undefined, Null
+ /*
+    PRIMITIVE DATA TYPES
+    
+    1. Number: Floating point numbers, for decimals and integers.
+    2. String: Sequence of characters, used for text.
+    3. Boolean: Logical data type that can only be true or false
+    4. Undefined: Data type of a variable that does not have a value yet.
+    5. Null: Also means 'not-existent'.
 
-var firstName = 'John';
-console.log(firstName);
+    Note: Javascript has dynamic typing: data types are automatically assigned to variables.
+ */
 
-var lastName = 'Smith'; // string
-var age = 28; // number
 
-var fullAge = true; // Boolean
-console.log(fullAge);
+ var firstName = 'John';
+ console.log(firstName);
+
+ var lastName = 'Smith'; // String
+ var age = 28; // Number
+
+ var fullAge = true; // Boolean
+ 
+ console.log(fullAge);
 
 var job; // Undefined
 console.log(job);
 
-job = 'teacher';
+job = 'Teacher';
 console.log(job);
 
-isMerried = null;
-console.log(isMerried); // Null
+
+// Variable naming rules
+var _years = 3;
+var johnMark = 'John and Mark';
+var if = 23;
