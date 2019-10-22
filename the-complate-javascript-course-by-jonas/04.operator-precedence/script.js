ba76d4fef14d411dda7912bc5704cd8b8d549eb0
Foysal Hossain <fimran2011@gmail.com>
2019-10-22 23:32:38 +0600

    22-10-2019

--- the-complate-javascript-course-by-jonas/04.operator-precedence/script.js
+++ the-complate-javascript-course-by-jonas/04.operator-precedence/script.js
@@ -1,19 +1,19 @@
-/**********************************
- * Operator Precedence
-*/
+/*******************
+ * Operator precedence
+ */
 
-var now = 2018;
-var yearJohn = 1989;
-var fullAge = 18;
+ var now = 2018;
+ var yearJohn = 1989;
+ var fullAge = 18;
 
-// Multiple operators
-var isFullAge = now - yearJohn >= fullAge; // true
+ // Multiple operators
+ var isFullAge = now - yearJohn >= fullAge;  // true
 console.log(isFullAge);
 
 // Grouping
 var ageJohn = now - yearJohn;
 var ageMark = 35;
-var avarage = (ageJohn + ageMark) / 2;
+var avarage = (ageJohn + ageJohn) / 2;
 console.log(avarage);
 
 // Multiple assignments
@@ -22,29 +22,14 @@ x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
 console.log(x, y);
 
 // More operators
+x *= 2;
+console.log(x);
+x += 10;
+console.log(x);
+x -=10;
+console.log(x);
 
-x = x * 2;
-console.log(x);
-x += 10; // x = x + 10
-console.log(x);
-x = x + 1;
-x += 1;
 x++;
 console.log(x);
 x--;
 console.log(x);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
