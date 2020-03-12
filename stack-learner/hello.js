668d47dce61abb6a3206a06e40088aec13c79e4f
Foysal Imran <fimran2011@gmail.com>
2020-03-12 12:14:15 +0600

    06. Hello world and code structure
    First day practice

---
+++ stack-learner/hello.js
@@ -0,0 +1,12 @@
+console.log('Hello world'); // string
+console.log('I love Bangladesh'); // String
+
+console.log(45); // Number
+console.log(30.4566); // Floating number
+console.log('My favorite number is: ' + 100); // addition operator
+console.log(100 + 100); // addition
+console.log(100 - 20); // Subtraction
+console.log(10 * 10); // Multiplication
+console.log(10 ** 10); // Exponentiation
+console.log(100 / 10); // Divition
+console.log(6000 % 6); // Modulus
