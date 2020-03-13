fbcc2bd559928741a84eff4f990d63721d65dcf5
Foysal Imran <fimran2011@gmail.com>
2020-03-13 06:09:41 +0600

    dataType

--- stack-learner/chapter_2/variables.js
+++ stack-learner/chapter_2/variables.js
@@ -8,3 +8,12 @@ console.log(name + ' knows javascript');
 console.log('His age is only ' + age);
 console.log(name + ' is creating a Javascript course for All');
 console.log('But his age is only ' + age);
+
+// best practice to declare variables
+
+var camaleCase = 'This is a camaleCase variable.';
+
+var mathNumber = 10;
+var accountNumberDetailId = 104;
+
+var account_number_details_id = 223;
