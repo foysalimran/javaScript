4e4f7d645a21020da2a2b65af87584e2da7bd2ae
Foysal Imran <fimran2011@gmail.com>
2020-03-13 06:57:03 +0600

    Number datatype

--- stack-learner/chapter_2/datatypes.js
+++ stack-learner/chapter_2/datatypes.js
@@ -1,1 +1,53 @@
- 
+// Premitive data type
+
+// Number
+// 1, 1.0, 335.444
+
+// String
+// 'This is an string type data type' + "This is also an string type data type", "3"
+
+// Boolean
+// True, False
+
+// Undefined
+
+// Null
+
+// Object data type
+// Array
+// Object
+// Function
+
+/**
+ * NUMBERS
+ */
+var intNumber = 2334; // Integer number
+var floNumber = 12.344; // Floating number
+console.log(intNumber);
+console.log(floNumber);
+
+console.log(Number(34)); // Construct integer number with number function
+console.log(Number('233')); // Construct float
+console.log(Number('This is a number')); // Construct string with number function // NaN
+
+// number parseFloat
+console.log(parseFloat(intNumber));
+console.log(parseFloat(floNumber));
+
+console.log(parseFloat('10'));
+console.log(parseFloat('10.22'));
+console.log(parseFloat('23 44 55'));
+console.log(parseFloat('23 years'));
+
+// Number parseFloat with variable
+var nn = 'my age is 44 years';
+var nn2 = '33 years';
+console.log(parseFloat(nn));
+console.log(parseFloat(nn2));
+
+// Max value and min safe vaiue
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_SAFE_INTEGER);
+
+// String multiply with number
+console.log(1 / 0); // Infinite
