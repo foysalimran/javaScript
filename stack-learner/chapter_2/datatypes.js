f96d0470a2bb181fc81149ad26579d7cd13339e2
Foysal Imran <fimran2011@gmail.com>
2020-03-14 17:57:59 +0600

    typeconversion

--- stack-learner/chapter_2/datatypes.js
+++ stack-learner/chapter_2/datatypes.js
@@ -84,3 +84,21 @@ console.log(str7);
 /**
  * Booleans
  */
+/*
+var b1 = true;
+var b2 = false;
+
+var b3 = Boolean(true);
+var b4 = Boolean(false);
+console.log(b1, b2, b3, b4);
+*/
+
+/**
+ * Null vs Undefined
+ */
+
+var abc;
+var xyz = null;
+
+console.log(abc);
+console.log(xyz);
