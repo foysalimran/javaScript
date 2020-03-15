4833aea681ec8ec2c55391afcdb78fe848fb711a
Foysal Imran <fimran2011@gmail.com>
2020-03-15 10:56:28 +0600

    15.3.2020

---
+++ jonas/jonas/1.js-basics/script.js
@@ -0,0 +1,78 @@
+/********************************
+ * Variables and data types
+ */
+/*
+var firstName = 'Foysal';
+console.log(firstName);
+
+var lastName = 'Imran';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+// Variable naming rules
+var _3year = 3;
+var johnMark = 'John and Mark';
+var if = 23;
+*/
+
+/**
+ * Variable mutation and type coercion
+ */
+/*
+var firstName = 'John';
+var age = 28;
+
+// Type coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'Teacher';
+isMarried = false;
+
+console.log(
+  firstName +
+    ' is a ' +
+    age +
+    ' year old ' +
+    job +
+    '. Is he merried? ' +
+    isMarried
+);
+
+console.log(
+  firstName +
+    ' is a ' +
+    age +
+    ' years old ' +
+    job +
+    '. is he merried? ' +
+    isMarried
+);
+
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(
+  firstName +
+    ' is a ' +
+    age +
+    ' years old ' +
+    job +
+    '. is he merried? ' +
+    isMarried
+);
+
+var lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName);
+*/
+
+// NEXT LECTURE: 07 about math operators
