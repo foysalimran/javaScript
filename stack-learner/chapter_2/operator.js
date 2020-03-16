e82c37f6e835bf2fadce429a898193d366811816
Foysal Imran <fimran2011@gmail.com>
2020-03-16 11:41:35 +0600

    16.3

--- stack-learner/chapter_2/operator.js
+++ stack-learner/chapter_2/operator.js
@@ -8,7 +8,7 @@ console.log(a % b);
 
 // Incremental Operator
 // Pre Incremental  - Post Incremental
-
+/*
 console.log(++a); // Pre Increment
 
 console.log(a++); // Post Increment
@@ -17,3 +17,42 @@ console.log(a);
 console.log(--b); // Pre decrement
 console.log(b--); //Post decrement
 console.log(b);
+*/
+
+/*
+// Assignment Operator
+var a = 10;
+var b = 20;
+
+a += b;
+
+console.log(a);
+
+a -= b;
+console.log(a);
+
+a *= b;
+console.log(a);
+
+a /= b;
+console.log(a);
+
+a %= b;
+console.log(a);
+*/
+
+// Comparison Operator
+
+var a = 10;
+var b = 20;
+
+console.log(a == b); // false
+console.log(a != b); // true
+
+console.log(a > b); // false
+console.log(a < b); // true
+
+a = 20;
+
+console.log(a >= b); // false
+console.log(a <= b); // true
