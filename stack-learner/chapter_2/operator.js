deeacb6cae6d5540243e3fddc61bebd605a036ed
Foysal Imran <fimran2011@gmail.com>
2020-03-16 11:29:29 +0600

    15.3.2020

---
+++ stack-learner/chapter_2/operator.js
@@ -0,0 +1,19 @@
+// Arithmetic Operator
+// + - * ** / % ++ --
+
+var a = 11;
+var b = 3;
+
+console.log(a % b);
+
+// Incremental Operator
+// Pre Incremental  - Post Incremental
+
+console.log(++a); // Pre Increment
+
+console.log(a++); // Post Increment
+console.log(a);
+
+console.log(--b); // Pre decrement
+console.log(b--); //Post decrement
+console.log(b);
