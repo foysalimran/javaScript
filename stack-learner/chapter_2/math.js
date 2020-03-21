45afde1a473f44a8da21cf7771a32d4206c65c11
Foysal Imran <fimran2011@gmail.com>
2020-03-21 17:46:42 +0600

    21.3.20202

--- stack-learner/chapter_2/math.js
+++ stack-learner/chapter_2/math.js
@@ -1,8 +1,18 @@
 console.log(Math.E);
 console.log(Math.PI);
 
+/**
+ *  Commonly used Math functions
+ */
 var n = 4.589;
 console.log(Math.abs(n));
 console.log(Math.floor(n));
 console.log(Math.ceil(n));
 console.log(Math.round(n));
+console.log(Math.max(400, 500, 399));
+console.log(Math.min(400, 500, 100));
+console.log(Math.pow(2, 3));
+console.log(Math.pow(3, 5));
+console.log(Math.sqrt(9));
+console.log(Math.sqrt(64));
+console.log(Math.round(Math.random() * 50 + 1));
