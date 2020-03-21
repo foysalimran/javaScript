fcc2dc657b1b447604d658b0915ed8ab34664140
Foysal Imran <fimran2011@gmail.com>
2020-03-21 17:57:51 +0600

    21.3.2020

--- stack-learner/chapter_2/variables.js
+++ stack-learner/chapter_2/variables.js
@@ -4,6 +4,7 @@ var age = 23;
 // console.log(name);
 // console.log(age);
 
+// this some lines will log some outputs
 console.log(name + ' knows javascript');
 console.log('His age is only ' + age);
 console.log(name + ' is creating a Javascript course for All');
@@ -17,3 +18,15 @@ var mathNumber = 10;
 var accountNumberDetailId = 104;
 
 var account_number_details_id = 223;
+
+/**
+ * This is a double
+ * line comment
+ */
+
+/*
+ this is a comment
+ This is another comment
+This is third line of comment
+Compiler will not read it
+*/
