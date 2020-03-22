bd6f0a7c24a53d9c3035e691ce0f2fa050d13ac3
Foysal Imran <fimran2011@gmail.com>
2020-03-22 21:21:36 +0600

    22.3.2020

---
+++ stack-learner/chapter_3/elseif.js
@@ -0,0 +1,26 @@
+var a = 20;
+var b = 20;
+
+// a > b true false
+
+if (a > b) {
+  console.log(' A is greater than B ');
+} else if (a < b) {
+  console.log('B is greater than A');
+} else {
+  console.log('They both are same');
+}
+
+// Find Even and Odd number
+
+var n = 1;
+
+console.log(n % 2);
+
+if (n == 0) {
+  console.log(n + ' is Zero');
+} else if (n % 2 == 0) {
+  console.log(n + ' is Even Number');
+} else {
+  console.log(n + ' is Odd Number');
+}
