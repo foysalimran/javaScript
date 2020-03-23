8896ebd3dc23a2ef5c2e0477226624bb2ee4b4eb
Foysal Imran <fimran2011@gmail.com>
2020-03-23 22:16:47 +0600

    23.3.2020

--- stack-learner/chapter_3/logicalOperator.js
+++ stack-learner/chapter_3/logicalOperator.js
@@ -12,11 +12,23 @@
 // false || true = true
 // false || false = false
 
-var a = 10;
+var a = 30;
 var b = 20;
 var c = 30;
 var d = 40;
 
 if (a > b && c > d) {
   console.log('A is greater than B and C is greater than D');
+} else {
+  console.log('At list one condition is false');
 }
+
+if (a > b || c > d) {
+  console.log('A is greater than B or C is greater than D');
+} else {
+  console.log('At list one condition is false');
+}
+
+var check = !(a > b);
+
+console.log(check);
