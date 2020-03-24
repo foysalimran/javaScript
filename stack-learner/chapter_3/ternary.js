5d00c6332404d2f2850c16637b2327d62a5b4dc6
Foysal Imran <fimran2011@gmail.com>
2020-03-24 22:57:51 +0600

    24.3.2020

---
+++ stack-learner/chapter_3/ternary.js
@@ -0,0 +1,15 @@
+var n = 10;
+
+var str = ' ';
+
+if (n % 2 == 0) {
+  str = 'EVEN';
+} else {
+  str = 'ODD';
+}
+console.log(str);
+
+var result = n % 2 == 0 ? 'EVEN' : 'ODD';
+console.log(result);
+
+// Condition ? true side : false side
