3fe9c4ce1ce04c74bb4e67ac80ce0bce0db82b4e
Foysal Imran <fimran2011@gmail.com>
2020-03-28 21:27:15 +0600

    28/3/2020

--- stack-learner/chapter_4/forloop.js
+++ stack-learner/chapter_4/forloop.js
@@ -1,9 +1,31 @@
 // For loops
 
-for (var i = 0; i < 100; i++) {
-  console.log(i + 1 + 'Foysal Imran');
-}
+// for (var i = 0; i < 100; i++) {
+//   console.log(i + 1 + 'Foysal Imran');
+// }
 
 // for (initializer, condition, increment) {
 //
 // }
+
+// print 1-100
+
+// for (var i = 1; i < 100; i++) {
+//   console.log(i);
+// }
+
+// Print odd numbers in 1-100
+
+// for (var i = 1; i < 100; i++) {
+//   if (i % 2 == 1) {
+//     console.log(i);
+//   }
+// }
+
+// Print even numbers in 1 - 100
+
+for (var i = 1; i < 101; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
