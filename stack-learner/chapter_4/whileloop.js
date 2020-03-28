5e0c89a91161d16564c811d24c4a8c7a6847e513
Foysal Imran <fimran2011@gmail.com>
2020-03-28 21:44:31 +0600

    28.3.2020

--- stack-learner/chapter_4/whileloop.js
+++ stack-learner/chapter_4/whileloop.js
@@ -1,5 +1,18 @@
-var i = 0;
-while (i < 10) {
-  console.log('Foysal Imran');
-  i++;
+// var i = 0;
+// while (i < 10) {
+//   console.log('Foysal Imran');
+//   i++;
+// }
+
+var isRunning = true;
+
+while (isRunning) {
+  var rand = Math.floor(Math.random() * 10 + 1);
+
+  if (rand == 9) {
+    console.log('Winner Winner Chicken Dinner');
+    isRunning = false;
+  } else {
+    console.log('You have got ' + rand + '.');
+  }
 }
